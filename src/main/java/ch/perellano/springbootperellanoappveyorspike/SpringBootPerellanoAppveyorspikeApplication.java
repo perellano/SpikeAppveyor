ea7b01eca66bc6355fd3c32db28b3606fdacf786
Gerald Perellano <pero@zhaw.ch>
2021-08-09 10:46:57 +0200

    initial config

---
+++ src/main/java/ch/perellano/springbootperellanoappveyorspike/SpringBootPerellanoAppveyorspikeApplication.java
@@ -0,0 +1,13 @@
+package ch.perellano.springbootperellanoappveyorspike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPerellanoAppveyorspikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPerellanoAppveyorspikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/perellano/springbootperellanoappveyorspike/SpringBootPerellanoAppveyorspikeApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.perellano.springbootperellanoappveyorspike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPerellanoAppveyorspikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
